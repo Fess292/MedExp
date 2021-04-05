2710c309a3eedef107187b0c035bdbce32275e4c
Fess292 <74976243+Fess292@users.noreply.github.com>
2021-04-05 20:57:42 +0300

    del-commit

--- js/script.js
+++ js/script.js
@@ -1,50 +1,4 @@
-// $(document).ready(function(){
-//   $('[data-modal=consultation]').on('click', function() {
-//      $('.overlay, #consultation').fadeIn('slow');
-//   });
-//   $(".modal__close").on('click', function() {
-//       $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-//   });
-
-//    $('.button_mini').each(function(i) {
-//        $(this).on('click', function() {
-//           $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); 
-//           $('.overlay, #order').fadeIn('slow');
-//        })
-//    });
-
-//    function validateForms(form) {
-//       $(form).validate({
-//           rules: {
-//               name: "required",
-//               phone: "required",
-//               email: {
-//                  required: true,
-//                  email:true
-//               }
-//           },
-//           messages: {
-//              name: "Пожалуйста, введите свое имя",
-//              phone: "Пожалуйста, введите свой телефон",
-//              email: {
-//                required: "Пожалуйста, введите свою почту",
-//                email: "Неправельной введен адрес почты,"
-//              }
-//            }
-//       });
-//    }
-   
-//    validateForms ('#consultation-form');
-//    validateForms ('#consultation form');
-//    validateForms ('#order form');
-
-//    $('input[name=phone]').mask("+7(999) 999-99-99");
-// });
-
-
-
 $(document).ready(function(){
-  // function validateForms('#myForm') {
   $("#myForm").validate({
     rules: {
       email: {
@@ -80,8 +34,6 @@ $(document).ready(function(){
         required: "Поле имя обязательно для заполнения",
         minlength:jQuery.validator.format("Длина имени должна быть больше 2-ти символов"),
         maxlength:jQuery.validator.format("Длина имени должна быть не больше 10-ти символов"),
-        // max: jQuery.validator.format("Длина имени должна быть не больше 10-ти символов"),
-        // min: jQuery.validator.format("Длина имени должна быть больше 2-ти символов")
       },
       phone: "Пожалуйста, введите свой телефон",
       pass: "Введите свой пароль",
@@ -102,34 +54,6 @@ $(document).ready(function(){
     }
   }); 
 
-  // function  countCharPass(val){
-  //   var leng = val.value.length;
-  //   if(leng >= 8){
-  //   $('.password-input').addClass('done');
-  //   console.log('length done');
-  //   }else{
-  //   $('.password-input').removeClass('done');
-  //   console.log('length error');
-  //   }
-    
-  //   if($('input[name="pass"]').val().trim() === $('input[name="pass2"]').val().trim() ){
-  //   $('.password-input2').addClass('done');
-  //   console.log('match done');
-  //   } else{
-  //   $('.password-input2').removeClass('done');
-  //   console.log('match error');
-  //   }
-
-  //   }
-    
-  // form.addEventListener('submit', function(){
-  //   let p1 = form.querySelector('[name=pass2]') // поля в текущей форме, а не из других
-  //   let p2 = form.querySelector('[name=pass]')
-  //   if (p1.value !== p2.value) {
-  //     // ...
-  //   }
-  // }); 
-
   $('.popup-open').click(function() {
 		$('.popup-fade').fadeIn();
 		return false;
@@ -140,7 +64,6 @@ $(document).ready(function(){
 		return false;
 	});        
  
-	// Закрытие по клавише Esc.
 	$(document).keydown(function(e) {
 		if (e.keyCode === 27) {
 			e.stopPropagation();
@@ -148,53 +71,5 @@ $(document).ready(function(){
 		}
 	});
 	
-	// Клик по фону, но не по окну.
-	// $('.popup-fade').click(function(e) {
-	// 	if ($(e.target).closest('.popup').length == 0) {
-	// 		$(this).fadeOut();					
-	// 	}
-  // });
 })
   
-//     // $('[data-modal=personal]').on('click', function() {
-//     //     $('#personal').fadeIn('slow');
-//     //  });
-//     //  $(".modal__close").on('click', function() {
-//     //      $('#personal').fadeOut('slow');
-//     //  });
- 
-//     // //   $('.button').each(function(i) {
-//     // //       $(this).on('click', function() {
-//     // //          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); 
-//     // //          $('.overlay, #order').fadeIn('slow');
-//     // //       })
-//     // //   });
- 
-//     //   function validateForms(form) {
-//     //      $(form).validate({
-//     //          rules: {
-//     //              name: "required",
-//     //              phone: "required",
-//     //              email: {
-//     //                 required: true,
-//     //                 email:true
-//     //              }
-//     //          },
-//     //          messages: {
-//     //             name: "Пожалуйста, введите свое имя",
-//     //             phone: "Пожалуйста, введите свой телефон",
-//     //             email: {
-//     //               required: "Пожалуйста, введите свою почту",
-//     //               email: "Неправельной введен адрес почты,"
-//     //             }
-//     //           }
-//     //      });
-//     //   }
-      
-//     //   validateForms ('#personal');
-//     //   validateForms ('#consultation form');
-//     //   validateForms ('#order form');
- 
-//     //   $('input[name=phone]').mask("+7(999) 999-99-99");       
-//   }
-// });
